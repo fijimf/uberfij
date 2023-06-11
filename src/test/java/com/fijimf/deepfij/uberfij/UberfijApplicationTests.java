c693afb1bcf398963b2be87b7fe92a84de754533
Jim <fijimf@gmail.com>
2023-06-11 17:19:03 -0400

    Fix integration test

--- src/test/java/com/fijimf/deepfij/uberfij/UberfijApplicationTests.java
+++ src/test/java/com/fijimf/deepfij/uberfij/UberfijApplicationTests.java
@@ -1,10 +1,17 @@
 package com.fijimf.deepfij.uberfij;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ActiveProfiles("test-containers")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class UberfijApplicationTests {
+
 
 	@Test
 	void contextLoads() {
